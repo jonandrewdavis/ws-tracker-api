3b0720bd33944f9eada661471e77ded6df6c714c
Andrew Davis <jonandrewdavis@gmail.com>
2026-03-12 01:02:31 -0400

    feat: add CORS to allow itch and other origins

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,30 @@ export interface Env {
 	TURN_SECRET_KEY: string;
 }
 
+const ALLOWED_ORIGINS = ['https://html-classic.itch.zone', 'https://androodev.com', 'https://www.androodev.com', 'https://bewe.me'];
+
+function handleCors(request: Request<unknown>, response: Response) {
+	const origin = request.headers.get('Origin');
+	if (origin && ALLOWED_ORIGINS.includes(origin)) {
+		response.headers.set('Access-Control-Allow-Origin', origin);
+	}
+	// Set other necessary CORS headers
+	response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	response.headers.set('Access-Control-Max-Age', '86400'); // Cache preflight requests for 24 hours
+	return response;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		if (request.method === 'OPTIONS') {
+			const response = new Response(null, {
+				status: 204, // No Content for OPTIONS request
+			});
+
+			return handleCors(request, response);
+		}
+
 		const url = new URL(request.url);
 
 		// Serve root-level static files (favicon.ico, robots.txt) directly.
@@ -23,16 +45,19 @@ export default {
 			try {
 				const response = await TurnHelper.generate(env.TURN_SECRET_KEY, env.TURN_SECRET_KEY);
 				if (response != null) {
-					return new Response(JSON.stringify(response), options);
+					const turnResponse = new Response(JSON.stringify(response), options);
+					return handleCors(request, turnResponse);
 				} else {
 					const nothing = { response: 'not found' };
-					return new Response(JSON.stringify(nothing), options);
+					var nothingResponse = new Response(JSON.stringify(nothing), options);
+					return handleCors(request, nothingResponse);
 				}
 			} catch {}
 		}
 
 		const options = { headers: { 'content-type': 'application/json' } };
 		const nothing = { response: 'not found' };
-		return new Response(JSON.stringify(nothing), options);
+		var response = new Response(JSON.stringify(nothing), options);
+		return handleCors(request, response);
 	},
 } satisfies ExportedHandler<Env>;
